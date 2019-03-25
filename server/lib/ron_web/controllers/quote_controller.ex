4c397ddd6a6b7e0003bee609ea3ba1b8debd4704
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-03-25 13:44:05 -0600

    sketch out quote vote and retrieval process

--- server/lib/ron_web/controllers/quote_controller.ex
+++ server/lib/ron_web/controllers/quote_controller.ex
@@ -1,7 +1,8 @@
 defmodule RonWeb.QuoteController do
   use RonWeb, :controller
 
-  def getQuote(conn, %{"size" => size} \\ "large") do
+  def getOne(conn, params) do
+    size = params[:size] || "large"
     {:ok, quote} = findQuote(size)
 
     conn
@@ -9,9 +10,19 @@ defmodule RonWeb.QuoteController do
       |> send_resp(200, quote)
   end
 
-  defp findQuote(size) do
-    {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body} =
-      :httpc.request(:get, {'http://ron-swanson-quotes.herokuapp.com/v2/quotes', []}, [], [])
+  def vote(conn, params) do
+    # if quote blacklist includes conn.remote_ip
+    # expects
+  end
+
+  defp findQuote(_size) do
+    # dets set
+    # get all the quotes
+    # send them to front end
+    # persist a quote if it is voted on
+    # persist an average rating
+    # key, quote, size, rating, blacklist
+    {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} = :httpc.request(:get, {'http://ron-swanson-quotes.herokuapp.com/v2/quotes', []}, [], [])
     [quote] = Jason.decode!(body)
     {:ok, quote}
   end
